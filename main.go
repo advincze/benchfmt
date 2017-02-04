f48628dd54223736169c8a4d8fb5230ff8859351
adam <advincze@gmail.com>
2017-02-04 15:31:33 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+import "os"
+import "bufio"
+import "strings"
+import "time"
+import "strconv"
+import "flag"
+
+func main() {
+	dur := flag.Duration("d", time.Second, "duration unit")
+	flag.Parse()
+
+	fmt.Println("bout")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// fmt.Println(line) // Println will add back the final '\n'
+		if strings.HasPrefix(line, "Benchmark") && strings.HasSuffix(line, "ns/op") {
+			fields := strings.Fields(line)
+			if len(fields) < 3 {
+				fmt.Println(line)
+				continue
+			}
+			nsPerOp, err := strconv.ParseInt(fields[2], 10, 64)
+			if err != nil {
+				fmt.Println(line)
+				continue
+			}
+			opsPerDur := dur.Nanoseconds() / nsPerOp
+			fmt.Printf("%s\t%d ops/%v\n", line, opsPerDur, dur)
+			continue
+		}
+		fmt.Println(line)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
